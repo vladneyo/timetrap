bb0a3cca537f09c5fa5bf8f1c145348d4072bfc9
Vladyslav Neichev <vlad27neychev95@gmail.com>
2025-03-30 01:53:57 +0200

    Simple implementation with printing in console

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+// use std::time::Instant;
+/// Trap method to wrap your code with
+///
+/// # Example
+/// ```
+/// use timetrap::trap;
+/// trap!({
+///     let a = 0;
+///     let b = a + 1;
+///     assert_eq!(b, a + 1);
+/// });
+/// ```
+#[macro_export]
+macro_rules! trap {
+    ($e:expr) => {
+        use std::time::Instant;
+        let start = Instant::now();
+        $e;
+        let duration = start.elapsed();
+        println!("{:?}", duration);
+    };
+}
